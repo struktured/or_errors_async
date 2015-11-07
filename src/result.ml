87b931ed69dbc7763d5e4a50e5f86f97cafbe78b
carm <carmelo.piccione@gmail.com>
2015-11-07 00:45:12 -0500

    partially written result

--- src/result.ml
+++ src/result.ml
@@ -1,10 +1,21 @@
 open Core.Std
 open Async.Std
-open Or_errors.Std
 
-include Deferred.Result
+module Monad_infix =
+struct
+  include Deferred.Result.Monad_infix
+  let (>|=) = (>>|)
+end
 
-let ok = is_ok
-let error = is_error
-let both x y = failwith("nyi")
+include (Deferred.Result : 
+  module type of Deferred.Result with module Monad_infix := Monad_infix)
+let ok t = failwith("nyi")
+(* try
+    let or_error = Deferred.Or_error.of_exn_result t in 
+    Some (Deferred.Or_error.ok_exn or_error)
+  with _ -> None *)
+let error t = failwith("nyi")
+let map f x = map x ~f
+let bind f x = bind x f
+let both x y = bind (fun x' -> map (fun y' -> (x', y')) y) x
 
