dcebddd08cdacc0a7b94721dbd0cf185db03f924
carm <carmelo.piccione@gmail.com>
2016-08-28 16:13:52 -0400

    fix pp/show impl location

--- src/async_or_error.ml
+++ src/async_or_error.ml
@@ -17,6 +17,11 @@ struct
   module Error = Async_error
   let of_result t = t
   let both (x:'a t) (y : 'b t) : ('a * 'b) t = Result.both x y
+   let pp a_ft ft = map ~f:(a_ft ft)
+    let show a_ft t = Format.flush_str_formatter() |>
+      fun (_:string) -> pp a_ft Format.str_formatter t |>
+                        map ~f:Format.flush_str_formatter
+
 end
 
 (* Ensure the implementation matches the OR_ERROR contract *)
@@ -24,10 +29,6 @@ module Signature : Or_errors.Std.OR_ERROR =
 struct
     include Impl
     open Async.Std
-    let pp a_ft ft = map ~f:(a_ft ft)
-    let show a_ft t = Format.flush_str_formatter() |>
-      fun (_:string) -> pp a_ft Format.str_formatter t |>
-                        map ~f:Format.flush_str_formatter
 end
 
 include Impl
